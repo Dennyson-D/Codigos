478b4e98f42eaef4217c5fc2d46a6a1c1ff7c85c
Dennyson-D <dennyson4@hotmail.com>
2024-05-06 22:32:38 -0300

    desafio3

---
+++ DIO - Dennyson/POO/avaliacao2_estrutura_de_dados.py
@@ -0,0 +1,43 @@
+# TODO: Crie a classe PlanoTelefone, seu método de inicialização e encapsule os atributos, 'nome' e 'saldo':
+class PlanoTelefone:
+    def __init__(self,nome,saldo):
+      self.nome = nome
+      self.saldo = saldo
+
+# TODO: Crie um método 'verificar_saldo' para verificar o saldo do plano sem acessar diretamente o atributo:    
+    def verificar_saldo(self):
+        return self.saldo
+# TODO: Crie um método 'mensagem_personalizada' para gerar uma mensagem personalizada com base no saldo: 
+    def mensagem_personalizada(self):
+        if self.saldo < 10:
+           msg = 'Seu saldo está baixo. Recarregue e use os serviços do seu plano.'
+        elif self.saldo >= 50:
+           msg = 'Parabéns! Continue aproveitando seu plano sem preocupações.'
+        else:
+           msg = 'Seu saldo está razoável. Aproveite o uso moderado do seu plano.'
+        return msg   
+
+# Classe UsuarioTelefone:
+class UsuarioTelefone:
+    def __init__(self, nome, plano):
+        self.nome = nome
+        self.plano = plano
+
+# TODO: Crie um método para verificar o saldo do usuário e gerar uma mensagem personalizada:
+    def verificar_saldo(self):
+        saldo =  self.plano.verificar_saldo()
+        mensagem = self.plano.mensagem_personalizada()
+        return saldo,mensagem
+
+# Recebendo as entradas do usuário (nome, plano, saldo):
+nome_usuario = input('nome_usuario')
+nome_plano = input('nome_plano')
+saldo_inicial = float(input('saldo_inicial'))
+
+ # Criação de objetos do plano de telefone e usuário de telefone com dados fornecidos:
+plano_usuario = PlanoTelefone(nome_plano, saldo_inicial) 
+usuario = UsuarioTelefone(nome_usuario, plano_usuario)  
+
+# Chamada do método para verificar_saldo sem acessar diretamente os atributos do plano:
+saldo_usuario, mensagem_usuario = usuario.verificar_saldo()  
+print(mensagem_usuario)
